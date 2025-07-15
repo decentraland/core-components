e35ab6fbde7cffb7d4ed1aebcc914581489716ae
Braian Mellor <braianj@gmail.com>
2025-07-15 17:54:55 -0300

    feat: export errors

--- components/slack/src/index.ts
+++ components/slack/src/index.ts
@@ -1,2 +1,3 @@
 export { createSlackComponent } from './component'
+export { SlackWebhookUrlError, SlackChannelError } from './errors'
 export type { SlackConfig, SlackMessage, ISlackComponent } from './types'
