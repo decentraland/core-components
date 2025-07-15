c3fe35707d5b2a7c1453ea5af23846de6e72d15a
Braian Mellor <braianj@gmail.com>
2025-07-15 17:57:46 -0300

    feat: remove unused errors

--- components/slack/src/index.ts
+++ components/slack/src/index.ts
@@ -1,3 +1,2 @@
 export { createSlackComponent } from './component'
-export { SlackWebhookUrlError, SlackChannelError } from './errors'
 export type { SlackConfig, SlackMessage, ISlackComponent } from './types'
